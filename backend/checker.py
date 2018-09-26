2a1a1af7f850f4dff4f16293d79aa358afda14f8
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-09-26 18:57:54 +0300

    CHECK

--- backend/checker.py
+++ backend/checker.py
@@ -6,20 +6,41 @@ text = raw["content"]
 groups_list = []
 output = {}
 
+teacher_list = ["Записной Д.В.", "Володин С.М.", "Юрченкова И.А.", "Сельченков Д.А.", "Семенихина А.В.", "Разин А.Л.", "Князева К.М.", "Филатов В.А.", "Поколодина Е.В.", "Леглер А.О.", "Перхункова Е.Д.", "Морозова М.В."]
+subjects_list = [""]
+
 groupflag = False
 thisgroupflag = False
 thisgroupnumper = 0
-
+grouplesoncounter = 1
 for line in text.splitlines():
     if (line!="") and (groupflag == False):
         groups_list = line.split(" ")
         groupflag = True
     elif (line.isdigit() == True):
-        thisgroupnumper = int(line)
         thisgroupflag = True
-        output[thisgroupnumper] = ""
+        thisgroupnumper = int(line)
+        grouplesoncounter = 0
+        output[thisgroupnumper] = {"0": {},
+                                   "1": {},
+                                   "2": {},
+                                   "3": {},
+                                   "4": {},
+                                   "5": {},
+                                   "6": {},
+                                   "7": {},
+                                   "8": {},
+                                   "9": {},
+
+            }
+    elif (line!="") and (thisgroupflag == True) and (line in teacher_list):
+        output[thisgroupnumper][str(grouplesoncounter)]["teacher"] = line+","     
     elif (line!="") and (thisgroupflag == True):
-        output[thisgroupnumper] += line
+        output[thisgroupnumper][str(grouplesoncounter)]["subject"] = line.replace("  ","")+","
+        grouplesoncounter += 1
+    
+    print(repr(line))
 
 print(groups_list)
+print(output[2])
 print(output[1])
---
+++ backend/test.py
@@ -0,0 +1,8 @@
+output = {}
+output[0] = {"1": {"teacher" : "КОТ КОТ", "projects" : "MEOW MEOW" },
+             "2" : {"teacher" : "КОТ КОТ", "projects" : "MEOW MEOW" }
+            }
+output[1] = {"teacher" : "КОТ", "projects" : "MEOW" }
+
+output[0]["1"]["teacher"] = "СОБАКАА"
+print(output[0])
