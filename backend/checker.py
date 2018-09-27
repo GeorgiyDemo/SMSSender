a94023eb162f918502c18cc8921be43ab6b8549b
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-09-27 13:43:41 +0300

    Пытаемся дальше распарсить

--- backend/checker.py
+++ backend/checker.py
@@ -7,12 +7,14 @@ groups_list = []
 output = {}
 
 teacher_list = ["Записной Д.В.", "Володин С.М.", "Юрченкова И.А.", "Сельченков Д.А.", "Семенихина А.В.", "Разин А.Л.", "Князева К.М.", "Филатов В.А.", "Поколодина Е.В.", "Леглер А.О.", "Перхункова Е.Д.", "Морозова М.В."]
-subjects_list = [""]
+subjects_list = ["Русскийязык", "Русскийязык", "Теориявероятностейиматематическаястатистика", "Иностранныйязык", "Иностранныйязык", "Иностранныиязык","Математика", "Инженернаяикомпьютернаяграфика","Теориявероятностейиматематическаястатистика","Эксплуатациякомпьютерныхсетей","Основыэкономики","Бухгалтерскийучет","Технологияразработкиизащитыбазданных","Эксплуатациякомпьютерныхсетей","ЭкономическиеосновыПОДФТ"]
 
 groupflag = False
 thisgroupflag = False
 thisgroupnumper = 0
 grouplesoncounter = 1
+
+newthisgroupline = ""
 for line in text.splitlines():
     if (line!="") and (groupflag == False):
         groups_list = line.split(" ")
@@ -27,20 +29,33 @@ for line in text.splitlines():
                                    "3": {},
                                    "4": {},
                                    "5": {},
-                                   "6": {},
-                                   "7": {},
-                                   "8": {},
-                                   "9": {},
+                                   
 
             }
     elif (line!="") and (thisgroupflag == True) and (line in teacher_list):
-        output[thisgroupnumper][str(grouplesoncounter)]["teacher"] = line+","     
-    elif (line!="") and (thisgroupflag == True):
-        output[thisgroupnumper][str(grouplesoncounter)]["subject"] = line.replace("  ","")+","
+        output[thisgroupnumper][str(grouplesoncounter)]["teacher"] = line 
+
+    elif (line!="") and (thisgroupflag == True) and (line in subjects_list):
+        output[thisgroupnumper][str(grouplesoncounter)]["subject"] = line.replace("  ","")
+        newthisgroupline = ""
         grouplesoncounter += 1
-    
-    print(repr(line))
+
+    elif (line!="") and (thisgroupflag == True) and (line in teacher_list == False) :
+
+        if newthisgroupline in subjects_list:
+            output[thisgroupnumper][str(grouplesoncounter)]["subject"] = line.replace("  ","")
+            newthisgroupline = ""
+            grouplesoncounter += 1
+        else:
+            newthisgroupline += line.replace(" ","").replace("     ","")
+            if newthisgroupline in subjects_list:
+                output[thisgroupnumper][str(grouplesoncounter)]["subject"] = line.replace("  ","")
+                newthisgroupline = ""
+                grouplesoncounter += 1
+            print(repr(newthisgroupline))
+            print("Русский" in subjects_list)
+            
 
 print(groups_list)
-print(output[2])
-print(output[1])
+print(output)
+print(output)
--- backend/test.py
+++ backend/test.py
@@ -6,3 +6,9 @@ output[1] = {"teacher" : "КОТ", "projects" : "MEOW" }
 
 output[0]["1"]["teacher"] = "СОБАКАА"
 print(output[0])
+
+from tika import parser
+import re
+raw = parser.from_file("sample.pdf")
+text = raw["content"]
+print(text)
