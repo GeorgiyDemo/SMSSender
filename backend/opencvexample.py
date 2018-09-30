739c4300c2e759ffbe6b0424a68711b34f5a82e9
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-09-30 14:52:56 +0300

    Ну, что-то начало получаться
    Тут кароч проверку на среднее расстояние между центрами прямоугольников с учетом того, что на первом шаге тоже может быть не особо правильное расположение (подумать еще)

--- backend/opencvexample.py
+++ backend/opencvexample.py
@@ -1,49 +1,74 @@
-import numpy as np
-import cv2
+import cv2, numpy
+from collections import Counter
+
+def centers_checker(a,b):
+    boolflag0 = False
+    boolflag1 = False
+    if (abs(a[0]-b[0]) < 5):
+        boolflag0 = True
+    if (abs(a[1]-b[1]) < 5):
+        boolflag1 = True
+    if boolflag1 == True and boolflag0 == True:
+        return True
+    return False
+
 
 color_blue = (255,0,0)
 color_red = (0,0,128)
-# загрузите изображение, смените цвет на оттенки серого и уменьшите резкость
+
+RowCheckerList=[]
+ColumnCheckerList=[]
+
 image = cv2.imread("example.jpg")
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv2.imwrite("gray.jpg", gray)
-# распознавание контуров
 edged = cv2.Canny(gray, 10, 250)
 cv2.imwrite("edged.jpg", edged)
-# создайте и примените закрытие
-#65 - где нет надписей
-#kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-#closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
-#cv2.imwrite("closed.jpg", closed)
-# найдите контуры в изображении и подсчитайте количество книг
 cnts = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[1]
-total = 0
-# цикл по контурам
+
+#Едем по контурам
+global_counter = 0
+old_center = (0, 0)
 for c in cnts:
-    # аппроксимируем (сглаживаем) контур
+
     rect = cv2.minAreaRect(c) # пытаемся вписать прямоугольник
     box = cv2.boxPoints(rect)
-    box = np.int0(box)
+    box = numpy.int0(box)
     center = (int(rect[0][0]),int(rect[0][1]))
     area = int(rect[1][0]*rect[1][1])
-
-    edge1 = np.int0((box[1][0] - box[0][0],box[1][1] - box[0][1]))
-    edge2 = np.int0((box[2][0] - box[1][0], box[2][1] - box[1][1]))
-
-    usedEdge = edge1
-    if cv2.norm(edge2) > cv2.norm(edge1):
-        usedEdge = edge2
-
-    reference = (1,0) # horizontal edge
-
-    if (area > 10000) and (area < 60000) and (rect[1][0] > 75):
+    if (area > 10000) and (area < 60000) and (rect[1][0] > 75) and (rect[1][1] > 75) and (centers_checker(center,old_center) == False):
         print(rect)
+        old_center = center
         cv2.drawContours(image,[box],0,color_blue,2)
         cv2.circle(image, center, 5, color_red, 2)
+        print("center: "+str(center))
+        #Закидываем center на проверку
+        ColumnCheckerList.append(center[0])
+        RowCheckerList.append(center[1])
+
         smallimg = cv2.resize(image, (0,0), fx=0.5, fy=0.5)
         cv2.imshow("CHECK",smallimg)
         cv2.waitKey(100000)
+        global_counter +=1
 
-# показываем результирующее изображение
-print("Я нашёл {0} книг на этой картинке".format(total))
+#Считаем кол-во повторений
+RowCounter = Counter(RowCheckerList)
+ColumnCounter = Counter(ColumnCheckerList)
+
+#Ищем максимальные элементы
+MaxRow = 0
+MaxColumn = 0
+for item in list(RowCounter):
+    if (RowCounter[item]>MaxRow):
+        MaxRow=RowCounter[item]
+
+for item in list(ColumnCounter):
+    if (ColumnCounter[item]>MaxColumn):
+        MaxColumn=ColumnCounter[item]
+
+print(MaxRow)
+print(MaxColumn)
+
+#Итоги
+print("Я нашёл {0} ужс".format(global_counter))
 cv2.imwrite("output.jpg", image)
