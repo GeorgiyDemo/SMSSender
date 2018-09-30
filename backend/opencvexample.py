a6be1dcf962931da6e9247bb454eb537dde3f533
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-09-30 13:29:57 +0300

    Отработало на 2 примерах из 3

--- backend/opencvexample.py
+++ backend/opencvexample.py
@@ -6,18 +6,17 @@ color_red = (0,0,128)
 # загрузите изображение, смените цвет на оттенки серого и уменьшите резкость
 image = cv2.imread("example.jpg")
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-gray = cv2.GaussianBlur(gray, (3, 3), 0)
 cv2.imwrite("gray.jpg", gray)
 # распознавание контуров
 edged = cv2.Canny(gray, 10, 250)
 cv2.imwrite("edged.jpg", edged)
 # создайте и примените закрытие
 #65 - где нет надписей
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
-cv2.imwrite("closed.jpg", closed)
+#kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+#closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
+#cv2.imwrite("closed.jpg", closed)
 # найдите контуры в изображении и подсчитайте количество книг
-cnts = cv2.findContours(closed.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[1]
+cnts = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[1]
 total = 0
 # цикл по контурам
 for c in cnts:
@@ -37,9 +36,13 @@ for c in cnts:
 
     reference = (1,0) # horizontal edge
 
-    if (area > 14000) and (area < 60000) and (rect[1][0] > 100):
+    if (area > 10000) and (area < 60000) and (rect[1][0] > 75):
+        print(rect)
         cv2.drawContours(image,[box],0,color_blue,2)
         cv2.circle(image, center, 5, color_red, 2)
+        smallimg = cv2.resize(image, (0,0), fx=0.5, fy=0.5)
+        cv2.imshow("CHECK",smallimg)
+        cv2.waitKey(100000)
 
 # показываем результирующее изображение
 print("Я нашёл {0} книг на этой картинке".format(total))
