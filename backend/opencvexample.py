2884b2602233fe382c88cb6d4071a33cbaf37e91
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-10-02 11:41:14 +0300

    Надо понять, как собрать матрицу по list с координатами (x,y)

--- backend/opencvexample.py
+++ backend/opencvexample.py
@@ -1,8 +1,19 @@
-import cv2, numpy, random
+import cv2, numpy, random, requests
 from collections import Counter
-from scipy.spatial import distance
+from pdf2image import convert_from_path
 
 circle_store_list = []
+
+def get_document():
+
+    url = 'http://178.128.225.114/PDF/4.pdf'
+    r = requests.get(url, stream=True)
+    with open('PDF.pdf', 'wb') as fd:
+        for chunk in r.iter_content(2000):
+            fd.write(chunk)
+    pages = convert_from_path('PDF.pdf', 150)
+    for page in pages:
+        page.save('JPG.jpg', 'JPEG')
 
 #Процедура для того, чтоб контур не был огромным
 def check_res(firstflag, old, new):
@@ -10,6 +21,23 @@ def check_res(firstflag, old, new):
         return True
     else:
         return new[1][0]<(old[1][0]+(old[1][0]/100)*80)
+
+#Процедура визаулизации матрицы
+def matrix(rows,columns):
+    matrix = []
+
+    counter = 0
+    print(len(circle_store_list))
+    for i in range(columns):
+        matrix.append([])
+        for j in range(rows):
+            matrix[i].append(0)
+            print(counter)
+            counter +=1
+    
+    print(matrix)
+            
+
 
 #Процедура дополнительной дорисовки кругов
 def circle_checker(rows,columns):
@@ -21,10 +49,11 @@ def circle_checker(rows,columns):
     print("------")
     for i in range(1,len(circle_store_list)):
         bufcheck = abs(circle_store_list[i-1][0]-circle_store_list[i][0])
+
         if bufcheck < 250:
             Xcircle.append(bufcheck)
         if (i%rows==0):
-            print(Xcircle) #здесь проверяем на макс длинну все Xcircle (как лучше??)
+            #print(Xcircle) #здесь проверяем на макс длинну все Xcircle (как лучше??)
             nparr = numpy.append(nparr, Xcircle)
             Xcircle = []
             #Ycircle.append(abs(circle_store_list[i][1]-circle_store_list[j][1]))
@@ -48,68 +77,73 @@ def centers_checker(a,b):
         return True
     return False
 
-color_red = (0,0,128)
 
-RowCheckerList=[]
-ColumnCheckerList=[]
-firstflag = True
+def main():
 
-image = cv2.imread("example.jpg")
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imwrite("gray.jpg", gray)
-edged = cv2.Canny(gray, 10, 250)
-cv2.imwrite("edged.jpg", edged)
-cnts = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[1]
+    get_document()
+    RowCheckerList=[]
+    ColumnCheckerList=[]
+    firstflag = True
 
-#Едем по контурам
-global_counter = 0
-old_center = (0, 0)
-old_rect = ((0.0, 0.0), (0.0, 0.0), -0.0)
-for c in cnts:
+    image = cv2.imread("JPG.jpg")
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite("gray.jpg", gray)
+    gray = cv2.GaussianBlur(gray, (3, 3), 0)
+    edged = cv2.Canny(gray, 10, 250)
+    cv2.imwrite("edged.jpg", edged)
+    cnts = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[1]
 
-    rect = cv2.minAreaRect(c) # пытаемся вписать прямоугольник
-    box = cv2.boxPoints(rect)
-    box = numpy.int0(box)
-    center = (int(rect[0][0]),int(rect[0][1]))
-    area = int(rect[1][0]*rect[1][1])
-    
-    if (check_res(firstflag,old_rect,rect) == True) and (area > 10000) and (area < 60000) and (rect[1][0] > 75) and (rect[1][1] > 75) and (centers_checker(center,old_center) == False):
+    #Едем по контурам
+    global_counter = 0
+    old_center = (0, 0)
+    old_rect = ((0.0, 0.0), (0.0, 0.0), -0.0)
+    for c in cnts:
+
+        rect = cv2.minAreaRect(c) # пытаемся вписать прямоугольник
+        box = cv2.boxPoints(rect)
+        box = numpy.int0(box)
+        center = (int(rect[0][0]),int(rect[0][1]))
+        area = int(rect[1][0]*rect[1][1])
         
-        firstflag = False
-        circle_store_list.append(center)
-        old_rect = rect
-        old_center = center
+        if (check_res(firstflag,old_rect,rect) == True) and (area > 10000) and (area < 60000) and (rect[1][0] > 75) and (rect[1][1] > 75) and (centers_checker(center,old_center) == False):
+            
+            firstflag = False
+            circle_store_list.append(center)
+            old_rect = rect
+            old_center = center
 
-        cv2.drawContours(image,[box],0,(random.randint(0,255),random.randint(0,255),random.randint(0,255)),2)
-        cv2.circle(image, center, 5, color_red, 2)
-        print(rect)
-        print("center: "+str(center))
-        #Закидываем центры на проверку для подсчета кол-ва повторений
-        ColumnCheckerList.append(center[0])
-        RowCheckerList.append(center[1])
+            cv2.drawContours(image,[box],0,(0,0,128),2)
+            cv2.circle(image, center, 5, (0,0,128), 2)
+            print(rect)
+            print("center: "+str(center))
+            #Закидываем центры на проверку для подсчета кол-ва повторений
+            ColumnCheckerList.append(center[0])
+            RowCheckerList.append(center[1])
 
-        smallimg = cv2.resize(image, (0,0), fx=0.5, fy=0.5)
-        cv2.imshow("CHECK",smallimg)
-        cv2.waitKey(100000)
-        global_counter +=1
+            smallimg = cv2.resize(image, (0,0), fx=0.5, fy=0.5)
+            cv2.imshow("CHECK",smallimg)
+            cv2.waitKey(100000)
+            global_counter +=1
 
-#Считаем кол-во повторений 
-RowCounter = Counter(RowCheckerList)
-ColumnCounter = Counter(ColumnCheckerList)
+    #Считаем кол-во повторений 
+    RowCounter = Counter(RowCheckerList)
+    ColumnCounter = Counter(ColumnCheckerList)
 
-#Ищем максимальные элементы в структурированных объектах
-MaxRow = 0
-MaxColumn = 0
-for item in list(RowCounter):
-    if (RowCounter[item]>MaxRow):
-        MaxRow=RowCounter[item]
-for item in list(ColumnCounter):
-    if (ColumnCounter[item]>MaxColumn):
-        MaxColumn=ColumnCounter[item]
+    #Ищем максимальные элементы в структурированных объектах
+    MaxRow = 0
+    MaxColumn = 0
+    for item in list(RowCounter):
+        if (RowCounter[item]>MaxRow):
+            MaxRow=RowCounter[item]
+    for item in list(ColumnCounter):
+        if (ColumnCounter[item]>MaxColumn):
+            MaxColumn=ColumnCounter[item]
 
-#if global_counter !=(MaxRow*MaxColumn):
-circle_checker(MaxRow,MaxColumn)
+    #if global_counter !=(MaxRow*MaxColumn):
+    circle_checker(MaxRow,MaxColumn)
+    matrix(MaxRow,MaxColumn)
+    #Итоги
+    print("{0} предметов".format(global_counter))
+    cv2.imwrite("output.jpg", image)
 
-#Итоги
-print("{0} предметов".format(global_counter))
-cv2.imwrite("output.jpg", image)
+main()
