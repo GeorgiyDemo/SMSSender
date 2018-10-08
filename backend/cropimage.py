2ac14a7a5cf6624358cad6f5cfb0591bbb7957ab
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-10-08 18:05:38 +0300

    Кароч теперь чекаем еще названия групп

--- backend/cropimage.py
+++ backend/cropimage.py
@@ -1,18 +1,17 @@
-import cv2, numpy, requests
+import cv2, numpy, random, requests
 from collections import Counter
 from pdf2image import convert_from_path
 
 circle_store_list = []
-group_cell_list = []
 finalmatrix = []
 
 def get_document():
 
-    #url = 'http://178.128.225.114/PDF/4.pdf'
-    #r = requests.get(url, stream=True)
-    #with open('PDF.pdf', 'wb') as fd:
-    #    for chunk in r.iter_content(2000):
-    #        fd.write(chunk)
+    url = 'http://178.128.225.114/PDF/4.pdf'
+    r = requests.get(url, stream=True)
+    with open('PDF.pdf', 'wb') as fd:
+        for chunk in r.iter_content(2000):
+            fd.write(chunk)
     pages = convert_from_path('PDF.pdf', 150)
     for page in pages:
         page.save('JPG.jpg', 'JPEG')
@@ -85,8 +84,7 @@ def get_null_values(timg,image):
         box = numpy.int0(box)
         center = (int(rect[0][0]),int(rect[0][1]))
         area = int(rect[1][0]*rect[1][1])
-
-        #Проверка на основные ячейки
+        
         if (check_res(firstflag,old_rect,rect) == True) and (area > 10000) and (area < 60000) and (rect[1][0] > 75) and (rect[1][1] > 75) and (centers_checker(center,old_center) == False):
             firstflag = False
             outchecklist.append(center)
@@ -120,12 +118,33 @@ def get_null_values(timg,image):
 
     print(finalmatrix)
 
-def main():
+#Процедура дополнительной дорисовки кругов (а надо ли?)
+def circle_checker(rows,columns):
 
-    get_document()
-    RowCheckerList=[]
-    ColumnCheckerList=[]
-    firstflag = True
+    Xcircle = []
+    Ycircle = []
+    nparr = numpy.array([])
+    print("Врубаем дорисовку центров на основе текущих..")
+    print("------")
+    for i in range(1,len(circle_store_list)):
+        bufcheck = abs(circle_store_list[i-1][0]-circle_store_list[i][0])
+
+        if bufcheck < 250:
+            Xcircle.append(bufcheck)
+        if (i%rows==0):
+            print(Xcircle) #здесь проверяем на макс длинну все Xcircle (как лучше??)
+            nparr = numpy.append(nparr, Xcircle)
+            Xcircle = []
+            #Ycircle.append(abs(circle_store_list[i][1]-circle_store_list[j][1]))
+    print("-----")
+    print(nparr)
+
+    print("\nКол-во строк: "+str(rows))
+    print("Кол-во колонок: "+str(columns))
+    #print(Ycircle)
+
+
+def main():
 
     image = cv2.imread("JPG.jpg")
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -137,74 +156,26 @@ def main():
     global_counter = 0
     old_center = (0, 0)
     old_rect = ((0.0, 0.0), (0.0, 0.0), -0.0)
-    
-    #Проверка на ячейки названий групп
     for c in cnts:
-    
+
         rect = cv2.minAreaRect(c)
         box = cv2.boxPoints(rect)
         box = numpy.int0(box)
         center = (int(rect[0][0]),int(rect[0][1]))
         area = int(rect[1][0]*rect[1][1])
         
-        if (rect[1][1] < 50) and (rect[1][1] > 17) and (rect[1][0] > 100):
-            cv2.drawContours(image,[box],0,(0,0,255),2)
-            cv2.circle(image, center, 5, (0,0,255), 2)
-            group_cell_list.append(rect)
-            print(rect)
-            print("center: "+str(center))
-            smallimg = cv2.resize(image, (0,0), fx=0.5, fy=0.5)
-            cv2.imshow("CHECK",smallimg)
-            cv2.waitKey(100000)
-    
-    for c in cnts:
 
-        rect = cv2.minAreaRect(c)
-        box = cv2.boxPoints(rect)
-        box = numpy.int0(box)
-        center = (int(rect[0][0]),int(rect[0][1]))
-        area = int(rect[1][0]*rect[1][1])
 
-        if (check_res(firstflag,old_rect,rect) == True) and (area > 10000) and (area < 60000) and (rect[1][0] > 75) and (rect[1][1] > 75) and (centers_checker(center,old_center) == False) and (allcenters_checker(center) == True): #and (abs(rect[2])==0)
-            
-            print(rect[2])
-            firstflag = False
-            circle_store_list.append(center)
-            old_rect = rect
-            old_center = center
-
-            cv2.drawContours(image,[box],0,(0,0,128),2)
-            cv2.circle(image, center, 5, (0,0,128), 2)
-            #print(rect)
-            #print("center: "+str(center))
+        cv2.drawContours(image,[box],0,(0,0,128),2)
+        cv2.circle(image, center, 5, (0,0,128), 2)
+        print(rect)
+        print("center: "+str(center))
             #Закидываем центры на проверку для подсчета кол-ва повторений
-            ColumnCheckerList.append(center[0])
-            RowCheckerList.append(center[1])
-
-            smallimg = cv2.resize(image, (0,0), fx=0.5, fy=0.5)
-            cv2.imshow("CHECK",smallimg)
-            cv2.waitKey(100000)
-            global_counter +=1
-
-    #Считаем кол-во повторений 
-    RowCounter = Counter(RowCheckerList)
-    ColumnCounter = Counter(ColumnCheckerList)
-
-    #Ищем максимальные элементы в структурированных объектах
-    MaxRow = 0
-    MaxColumn = 0
-    for item in list(RowCounter):
-        if (RowCounter[item]>MaxRow):
-            MaxRow=RowCounter[item]
-    for item in list(ColumnCounter):
-        if (ColumnCounter[item]>MaxColumn):
-            MaxColumn=ColumnCounter[item]
-    
-    print(MaxRow*MaxColumn)
-    print(global_counter)
-    if global_counter == MaxRow*MaxColumn:
-        matrix(MaxRow,MaxColumn)
-        get_null_values(edged.copy(),image)
+          
+        smallimg = cv2.resize(image, (0,0), fx=0.5, fy=0.5)
+        cv2.imshow("CHECK",smallimg)
+        cv2.waitKey(100000)
+        global_counter +=1
 
     cv2.imwrite("output.jpg", image)
 
