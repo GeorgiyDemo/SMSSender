8df9dd1b487470d7576a016aa296bdb1f193815e
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-10-23 18:11:09 +0300

    Хорошее форматирование и вывод на DataGrid

--- SMSTimetable/SMSTimetable/SMSStatusClass.cs
+++ SMSTimetable/SMSTimetable/SMSStatusClass.cs
@@ -1,11 +1,6 @@
 ﻿using Newtonsoft.Json.Linq;
-using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 
 namespace SMSTimetable
 {
@@ -15,42 +10,44 @@ namespace SMSTimetable
         public string[,] GetSMSInfoAPI()
         {
 
+            var StatusDictionary = new Dictionary<string, string>
+            {
+                {"0", "в очереди"},
+                {"1", "доставлено"},
+                {"2", "не доставлено"},
+                {"3", "передано"},
+                {"4", "ожидание статуса сообщения"},
+                {"6", "сообщение отклонено"},
+                {"8", "на модерации"}
+            };
 
             SMSSenderClass SMSHistory_obj = new SMSSenderClass();
             dynamic HistoryJSON = JObject.Parse(SMSHistory_obj.sms_list(new Request { page = 1 }));
-            int length = HistoryJSON.data.Count;
-            MessageBox.Show(length.ToString());
-            string[,] array = new string[5, 5];
-            for (int i = 0; i < 5; i++)
+            string[,] array = new string[10, 5];
+            for (int i = 0; i < 10; i++)
             {
                 JObject ThisSMS = HistoryJSON.data[i.ToString()];
                 array[i, 0] = (i + 1).ToString();
                 array[i, 1] = ThisSMS["number"].ToString();
-                array[i, 2] = ThisSMS["status"].ToString();
-                array[i, 3] = ThisSMS["cost"].ToString();
-                array[i, 4] = ThisSMS["text"].ToString();
+                array[i, 2] = StatusDictionary[ThisSMS["status"].ToString()];
+                array[i, 3] = ThisSMS["cost"].ToString()+" руб.";
+                array[i, 4] = ThisSMS["text"].ToString().Replace("\n"," ");
 
             }
-            MessageBox.Show(array.ToString());
 
-
-               // MessageBox.Show((i + 1).ToString() + " +" + HistoryJSON["number"] + " \"" + HistoryJSON["text"] + "\"\n\n");
-            
-
-          
             return array;
         }
 
         public DataTable DataGridInput()
         {
             string[,] StringArray = GetSMSInfoAPI();
-
+            string[] FormatArray = { "№", "Телефон", "Статус", "Стоимость", "Текст" };
             var rows = StringArray.GetLength(0);
             var columns = StringArray.GetLength(1);
 
             var t = new DataTable();
             for (var c = 0; c < columns; c++)
-                t.Columns.Add(new DataColumn(c.ToString()));
+                t.Columns.Add(new DataColumn(FormatArray[c]));
 
             for (var r = 0; r < rows; r++)
             {
